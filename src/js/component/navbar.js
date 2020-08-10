377029aa1aa981a157c29c41be4efc93664df88f
luigiRP <luigirosales84@gmail.com>
2020-08-10 15:24:21 +0000

    navbar hecho

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div className="d-flex row justify-content-between border border-bottom bg-light">
+			<img
+				className="mx-5 my-3"
+				src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png"
+				alt="starWars"
+				width="70"
+				height="70"
+			/>
+			<Dropdown className="mx-5 my-3">
+				<Dropdown.Toggle variant="success" id="dropdown-basic">
+					Favorites
+				</Dropdown.Toggle>
+
+				<Dropdown.Menu>
+					<Dropdown.Item href="#/action-1">
+						Luke <i className="fas fa-trash mx-3" />
+					</Dropdown.Item>
+					<Dropdown.Item href="#/action-2">
+						Vader <i className="fas fa-trash mx-3" />
+					</Dropdown.Item>
+					<Dropdown.Item href="#/action-3">
+						R2D2 <i className="fas fa-trash mx-3" />
+					</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+		</div>
 	);
 };
