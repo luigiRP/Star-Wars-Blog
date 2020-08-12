e1897f1f700d9ff51f5acaa36ab617c09835abe9
riduarte <riduarte88@gmail.com>
2020-08-12 10:19:36 +0000

    People fecth

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,13 +1,31 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			starShips: []
+			starShips: [],
+			people: []
 		},
 		actions: {
 			getVehicles: async () => {
 				let response = await fetch("https://swapi.dev/api/starships/");
 				let newStarShips = await response.json();
 				setStore({ starShips: newStarShips.results });
+			},
+			getCharacter: () => {
+				fetch("https://swapi.dev/api/people/")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						// Read the response as json.
+						return response.json();
+					})
+					.then(function(responseAsJson) {
+						// Do stuff with the JSON
+						setStore({ people: responseAsJson.results });
+					})
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
 			}
 		}
 	};
